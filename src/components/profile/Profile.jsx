87cf45b47ad13ed9a170b65064e30ab82752ddca
Baljinder-Singh <baljindersingh987209@gmail.com>
2025-06-24 23:48:57 +0530

    fix: adjust padding in Profile component for improved layout

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Profile() {
   return (
-    <div className="profile pt-30 sm:pt-7 lg:pt-25 ">
+    <div className="profile pt-10 sm:pt-7 lg:pt-25 ">
       <div className="container flex flex-col lg:flex-row !p-0  ">
         <div className="left lg:w-1/2  px-8 flex flex-col w-full items-center justify-center space-y-5  min-h-[60vh] ">
           <p className="welcome text-3xl leading-14 lg:text-5xl  font-bold text-gray-800">Welcome to my Web Development Portofolio!</p>
